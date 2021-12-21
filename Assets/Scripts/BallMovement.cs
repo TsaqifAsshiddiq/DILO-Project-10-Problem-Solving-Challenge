033fa6056de3bbc061063aa02071cc6e36fd6b00
Maskip Studio <61724152+TsaqifAsshiddiq@users.noreply.github.com>
2021-12-21 20:17:55 +0700

    Add box

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
+    private Rigidbody2D ballRb;
     public float speed = 30.0f;
-    private Vector3 direction;
+    private Vector2 direction;
 
     // Start is called before the first frame update
     void Start()
     {
+        ballRb = GetComponent<Rigidbody2D>();
         direction = Random.insideUnitCircle.normalized;
+        PushBall();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(direction * Time.deltaTime * speed);
+
+    }
+
+    void PushBall()
+    {
+        ballRb.AddForce(direction * speed);
     }
 }
