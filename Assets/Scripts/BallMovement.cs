9a8804eb7bf645e76db48e212ad7eb4d1d7cd98a
Maskip Studio <61724152+TsaqifAsshiddiq@users.noreply.github.com>
2021-12-21 14:44:57 +0700

    Add move ball

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float speed = 30.0f;
+    private Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Random.insideUnitCircle.normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime * speed);
+    }
+}
