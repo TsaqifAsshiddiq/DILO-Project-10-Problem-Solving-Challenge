317b1989ab8ae8b78f29df28f2c50b2dc94cbf90
Maskip Studio <61724152+TsaqifAsshiddiq@users.noreply.github.com>
2021-12-22 13:56:49 +0700

    Add spawner

---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public GameObject boxPrefabs;
+    private float spawnRangeX = 9.0f;
+    private float spawnRangeY = 3.5f;
+    public int maxItem = 10;
+    private int currentItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentItem < maxItem)
+        {
+            SpawnBox();
+        }
+    }
+
+    void SpawnBox()
+    {
+        Vector2 spawnPos = new Vector2(Random.Range(-spawnRangeX, spawnRangeX), Random.Range(-spawnRangeY, spawnRangeY));
+        Instantiate(boxPrefabs, spawnPos, boxPrefabs.transform.rotation);
+        currentItem ++;
+    }
+}
