87e5329f64520fc6b222c95c4d3797b599283054
Maskip Studio <61724152+TsaqifAsshiddiq@users.noreply.github.com>
2021-12-22 13:33:42 +0700

    Add ball controller

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -5,24 +5,35 @@ using UnityEngine;
 public class BallMovement : MonoBehaviour
 {
     private Rigidbody2D ballRb;
-    public float speed = 30.0f;
-    private Vector2 direction;
+    public float speed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         ballRb = GetComponent<Rigidbody2D>();
-        direction = Random.insideUnitCircle.normalized;
-        PushBall();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.UpArrow)){
+            PushBall(Vector2.up);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            PushBall(Vector2.down);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            PushBall(Vector2.right);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            PushBall(Vector2.left);
+        }
     }
 
-    void PushBall()
+    void PushBall(Vector2 direction)
     {
         ballRb.AddForce(direction * speed);
     }
